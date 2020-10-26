e1328078c9bf40822f0957955f07c400ae17e783
erikamuller20 <73198437+erikamuller20@users.noreply.github.com>
2020-10-26 10:48:33 -0300

    Add project files.

---
+++ Ejercicio2/DivisionCero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class DivisionCero
+    {
+        public void Division(int i, int j)
+        {
+            try
+            {
+                int division = i / j;
+                Console.WriteLine($"El resultado es { division }");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"Error division entre cero. Mensaje: { ex.Message }");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Mensaje: { ex.Message }");
+            }
+            finally
+            {
+                Console.WriteLine("Ejecucion Realizada");
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+}
---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+             DivisionCero divisionCero = new DivisionCero();
+
+              divisionCero.Division(1, 0);
+              
+        }
+
+       
+    }
+}
